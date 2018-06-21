72a0fdbd1348131417539aa40d4a3dd65298086c
chengyudeng <1456694385@qq.com>
2018-06-21 23:39:08 +0800

    前后端联调完成

--- src/Auth.js
+++ src/Auth.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { login } from './Auth.redux';
+import { login, getUserData } from './Auth.redux';
 
 // 两个reducers 每个reducers都有一个state
 // 合并reducers
 @connect(
     state => state.auth,
-    { login }
+    { login, getUserData }
 )
 class Auth extends Component {
+    // constructor (props) {
+    //     super(props);
+    //     this.state = {
+    //         data: {}
+    //     }
+    // }
+    componentDidMount () {
+        this.props.getUserData();
+        // axios.get('/data')
+        //     .then(res => {
+        //         if (res.status === 200) {
+        //             this.setState({
+        //                 data: res.data
+        //             });
+        //         }
+        //         console.log(res);
+        //     })
+    }
     render () {
         return (
             <div>
+                <h2>我的名字是{ this.props.user }, 年龄{ this.props.age }</h2>
                 { this.props.isAuth ? <Redirect to="/dashboard" /> : null  }
                 <h2>你没有权限, 需要登陆才能看</h2>
                 <button onClick={ this.props.login }>登陆</button>
--- src/Auth.redux.js
+++ src/Auth.redux.js
@@ -1,15 +1,43 @@
+import Axios from "axios";
+
 const  LOGIN = 'LOGIN';
 const LOGOUT = 'LOGOUT';
+const USER_DATA = 'USER_DATA';
 
-export function auth (state={ isAuth: false, user: '李云龙' }, action) {
+const initState = {
+    isAuth: false,
+    user: '李云龙',
+    age: 20
+}
+
+export function auth (state=initState, action) {
+    console.log(state, action);
     switch (action.type) {
         case LOGIN:
             return { ...state, isAuth: true };
         case LOGOUT:
             return { ...state, isAuth: false };
+        case USER_DATA:
+            return { ...state, user: action.payload.user, age: action.payload.age }
         default: 
              return state;
     }
+}
+
+export function getUserData () {
+    // dispatch用来通知数据修改
+    return dispatch => {
+        Axios.get('/data')
+            .then(res => {
+                if (res.status === 200) {
+                    dispatch(userData(res.data));
+                }
+            })
+    }
+}
+
+export function userData(data) {
+    return { type: USER_DATA, payload: data }
 }
 
 export function login () {
